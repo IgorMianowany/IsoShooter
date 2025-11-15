f8c46135687fe9b3a2a89a5d4fa2482d2d7d9f43
Igor Mianowany <fimiany@gmail.com>
2025-11-16 00:10:44 +0100

    added back shotgun png, made shotgun shoot more bullets

--- shotgun.gd
+++ shotgun.gd
@@ -4,7 +4,7 @@ extends Gun
 var fire_rate : float = 1
 
 @onready var gun_anim : AnimationPlayer = $AnimationPlayer
-@onready var gun_barrel = $RayCast3D
+@onready var gun_barrel = $Barrel
 
 func _process(delta: float) -> void:
 	fire_rate -= delta
@@ -13,8 +13,9 @@ func _shoot():
 	if !gun_anim.is_playing() and fire_rate <= 0:
 		fire_rate = 1
 		gun_anim.play("shoot")
-		instance = bullet.instantiate()
-		player.get_parent().add_child(instance)
-		instance.damage = 5
-		instance.global_position = gun_barrel.global_position
-		instance.transform.basis = gun_barrel.global_transform.basis
+		for child in gun_barrel.get_children():
+			instance = bullet.instantiate()
+			player.get_parent().add_child(instance)
+			instance.damage = 1
+			instance.global_position = child.global_position
+			instance.transform.basis = child.global_transform.basis
