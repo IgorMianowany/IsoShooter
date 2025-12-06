1fd879167323937b76e63094c5ad1722fe8f6144
Igor Mianowany <fimiany@gmail.com>
2025-12-06 12:10:11 +0100

    optimization

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -6,7 +6,7 @@ var speed : float = 500
 const JUMP_VELOCITY = 4.5
 var health = 10
 var pursue_cooldown : float = 0
-var pursue_cooldown_max : float = 1
+var pursue_cooldown_max : float = .5
 var player : Player
 var next_nav_position : Vector3
 var new_velocity : Vector3
@@ -17,6 +17,8 @@ var is_attacking : bool = false
 var is_idle : bool = true
 var is_reaction : bool = false
 var is_hard_reaction : bool = false
+var update_frame_offset : int =  0
+var frames_since_update : int = 0
 
 @export var player_path : NodePath
 
@@ -27,12 +29,15 @@ func _ready() -> void:
 	healthbar.init_healthbars(health)
 	previous_position = global_position
 	nav_agent.avoidance_priority = 1 - randf_range(0, 0.4)
+	pursue_cooldown_max += randf_range(0,.25)
 
 func _physics_process(delta: float) -> void:
+	frames_since_update += 1
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-	if player != null:
+	if player != null and frames_since_update > update_frame_offset:
+		frames_since_update = 0
 		velocity = Vector3.ZERO
 		if pursue_cooldown <= 0:
 			nav_agent.set_target_position(player.global_transform.origin)
@@ -42,19 +47,20 @@ func _physics_process(delta: float) -> void:
 
 		velocity = new_velocity
 		look_at_pos = global_position + velocity
-		look_toward_pos = look_toward_pos.move_toward(Vector3(look_at_pos.x, global_position.y + 5, look_at_pos.z), delta * 30)
 		# look at movement direction
-		if look_toward_pos.x != global_position.x or look_toward_pos.z != global_position.z:
-			look_at(Vector3(look_toward_pos.x, global_position.y, look_toward_pos.z))
+
 		# look at player
 		#look_at(Vector3(player.global_position.x, global_position.y, player.global_position.z))
+	look_toward_pos = look_toward_pos.move_toward(Vector3(look_at_pos.x, global_position.y + 5, look_at_pos.z), delta * 30)
+	if look_toward_pos.x != global_position.x or look_toward_pos.z != global_position.z:
+		look_at(Vector3(look_toward_pos.x, global_position.y, look_toward_pos.z))
 	move_and_slide()
 	
 func _process(delta: float) -> void:
 	pursue_cooldown -= delta
 
 func _on_area_3d_body_part_hit(damage: Variant) -> void:
-	var reaction_roll = randf_range(0,1)
+	var reaction_roll: float = randf_range(0,1)
 	if reaction_roll >= .9:
 		speed = 0
 		if reaction_roll > .975: 
@@ -71,8 +77,6 @@ func _on_area_3d_body_part_hit(damage: Variant) -> void:
 func _on_aggro_range_area_entered(area: Area3D) -> void:
 	player = area.get_parent()
 	is_idle = false
-	print("cos")
-
 
 func _on_navigation_agent_3d_velocity_computed(safe_velocity: Vector3) -> void:
 	velocity = safe_velocity
--- scripts/enemy_spawn.gd
+++ scripts/enemy_spawn.gd
@@ -6,9 +6,11 @@ var enemy_scene := preload("res://enemy.tscn")
 @onready var possible_spawns = $PossibleSpawns
 
 func spawn_enemy():
+	var offset = randi_range(15,25)
 	for child in possible_spawns.get_children():
-		if child.entity_counter == 0 and EventBus.enemy_count < 10:
+		if child.entity_counter == 0 and EventBus.enemy_count < 100:
 			var enemy : Enemy = enemy_scene.instantiate()
+			enemy.update_frame_offset = offset
 			get_parent().add_child(enemy)
 			enemy.global_position = child.global_position
 			EventBus.enemy_spawned.emit()
