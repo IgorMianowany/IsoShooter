86b676f7c70813647c134d2894b061e6f4b1ed17
Igor Mianowany <fimiany@gmail.com>
2025-12-06 13:31:21 +0100

    prevented the same hitbox from hitting multiple times

--- scripts/hitbox.gd
+++ scripts/hitbox.gd
@@ -1,4 +1,13 @@
 class_name Hitbox
 extends Area3D
 
+signal hit
+
 var damage : float = 10
+
+func _ready() -> void:
+	hit.connect(handle_hit)
+
+func handle_hit():
+	set_deferred("monitorable", false)
+	
--- scripts/hurtbox.gd
+++ scripts/hurtbox.gd
@@ -3,4 +3,5 @@ extends Area3D
 
 
 func _on_area_entered(area: Area3D) -> void:
+	(area as Hitbox).hit.emit()
 	owner.take_damage(area.damage, area.owner.global_position)
